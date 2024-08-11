2f820cabbbf8e4125c118ab002c9789191b8eef8
Malcolm Busfield <mbusfield@trimetrauk.onmicrosoft.com>
2024-08-11 18:39:54 +0100

    initial commit

---
+++ RonCoach101/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RonCoach101;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ RonCoach101/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RonCoach101;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
